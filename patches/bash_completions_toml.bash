e9a03ebc0b7342dfcd6a72eccf6b6a5eb62fcd0b
bib0x <bib0x+github@bib0x.net>
2024-02-27 21:51:36 +0100

    new bash_completions patch

---
+++ patches/bash_completions_toml.bash
@@ -0,0 +1,20 @@
+diff --git a/completions/ksearch.bash b/completions/ksearch.bash
+index b0929cc..a7ed8c6 100644
+--- a/completions/ksearch.bash
++++ b/completions/ksearch.bash
+@@ -33,12 +33,9 @@ _ksearch() {
+                     COMPREPLY=($(compgen -f "${cur}"))
+                     return 0
+                     ;;
+-                --topic)
+-                    COMPREPLY=($(compgen -f "${cur}"))
+-                    return 0
+-                    ;;
+-                -t)
+-                    COMPREPLY=($(compgen -f "${cur}"))
++                -t|--topic)
++		    files=$(IFS=:; for path in $KSEARCH_PATH; do find -L $path -type f -name "$cur*.toml" -printf '%f\n'; done | sort -u | sed -E 's/\.toml$//g')
++                    COMPREPLY=($(compgen -f "${files}" -- ${cur}))
+                     return 0
+                     ;;
+                 --filter)
